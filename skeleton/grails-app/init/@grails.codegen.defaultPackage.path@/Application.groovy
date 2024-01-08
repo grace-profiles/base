794325b6cd2fc6c172c8aeea0e0fd5f80af562c3
Michael Yan <rain@rainboyan.com>
2024-01-09 06:42:41 +0800

    Update skeleton

--- skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
+++ skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
@@ -1,12 +1,13 @@
 package @grails.codegen.defaultPackage@
 
 import grails.boot.Grails
-
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class Application {
+
     static void main(String[] args) {
         Grails.run(Application, args)
     }
+
 }
