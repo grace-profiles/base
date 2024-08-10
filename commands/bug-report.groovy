f14bf5fd7c06180c7862a9f9f0db63a0e1a85045
Michael Yan <rain@rainboyan.com>
2024-08-11 02:43:17 +0800

    Update `bug-report` command

--- commands/bug-report.groovy
+++ commands/bug-report.groovy
@@ -13,8 +13,20 @@ String zipName = "${buildDir}/${fileName}-bug-report-${date}.zip"
 ant.zip(destfile: zipName, filesonly: true) {
     fileset(dir: baseDir.canonicalPath) {
         include name: 'app/**'
+        include name: 'buildSrc/**'
+        include name: 'db/**'
+        include name: 'gradle/**'
         include name: 'src/**'
         include name: 'build.gradle'
+        include name: 'gradlew'
+        include name: 'gradlew.bat'
+        include name: 'gradle.properties'
+        include name: 'settings.gradle'
+        include name: '.gitignore'
+        include name: '.gitattributes'
+        exclude name: '**/.git/**'
+        exclude name: '**/.gradle/**'
+        exclude name: '**/build/**'
     }
 }
 
